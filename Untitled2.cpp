872ad907bdc432c9f4b91e9a141672cf12d78418
cobeirox <77794622+coberox@users.noreply.github.com>
2021-04-29 13:28:59 -0500

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+#include<conio.h>
+#include<windows.h>
+#include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+
+main(){
+
+	int N1 , N2 resul;
+    cout<<"ingrese el primer numero";
+    cin>>N1;
+    cout<<"ingrese el segundo numero";
+    cin>>N2;
+    
+    resul=N1+N2; 
+    cout<<"su resultado va hace una suma y es: "<< resul;
+    system("pause")
+
+}
+
